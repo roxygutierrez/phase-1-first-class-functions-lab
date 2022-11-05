744eb2ccd6bd3a1bebdaff37c580a1c6032c45ba
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-11-04 21:39:36 -0500

    commit tree 7da0e6fb92c6a78ed09a0bd30bdf266ecaa74f67

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiple) {
+  return function (fare) {
+    return fare * multiple;
+  };
+}
+
+const fareDoubler = (fare) => {
+  const doubler = createFareMultiplier(2);
+  return doubler(fare);
+};
+
+const fareTripler = (price) => {
+  const tripler = createFareMultiplier(3);
+  return tripler(price);
+};
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+  return func(arrayOfDrivers);
+}
